793ff4ffad26497a1648a6694d4e6a0fac23f80e
Patrick Kerby <patrick@kerbydesign.com>
2019-02-04 21:28:03 -0700

    add theme support for post thumbnails

--- app/setup.php
+++ app/setup.php
@@ -81,6 +81,7 @@ add_action('after_setup_theme', function () {
     add_theme_support( 'wc-product-gallery-zoom' );
     add_theme_support( 'wc-product-gallery-lightbox' );
     add_theme_support( 'wc-product-gallery-slider' );
+    add_theme_support( 'post-thumbnails' );
 
 }, 20);
 
