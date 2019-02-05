fa6804166f3bca5e3d2067401d7a64e2a9986103
Patrick Kerby <patrick@kerbydesign.com>
2019-02-04 21:32:59 -0700

    Featured image issues - ad post types to theme support

--- app/setup.php
+++ app/setup.php
@@ -81,8 +81,11 @@ add_action('after_setup_theme', function () {
     add_theme_support( 'wc-product-gallery-zoom' );
     add_theme_support( 'wc-product-gallery-lightbox' );
     add_theme_support( 'wc-product-gallery-slider' );
-    add_theme_support( 'post-thumbnails' );
-
+    add_theme_support('post-thumbnails', array(
+        'post',
+        'page',
+        'product',
+        ));
 }, 20);
 
 /**
