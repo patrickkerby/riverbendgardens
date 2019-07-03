5e38f40a42b24bdf91d584018706c28985181f95
Patrick Kerby <patrick@kerbydesign.com>
2019-07-03 08:28:26 -0600

    fix js conflict - footable vs week select 1.1

--- resources/views/lists-single.blade.php
+++ resources/views/lists-single.blade.php
@@ -84,23 +84,21 @@ $week15 = 'Week 15: ' . $week15_row['week'];
 @section('content')
   <script type="text/javascript">
 
-    (function($){
-      $('.table').footable();
-    }) ( jQuery );  
-  
-    ( function($) {
-        $(document).ready(function(){
-          $("select").change(function(){
-            $(this).find("option:selected").each(function(){
-              var optionValue = $(this).attr("value");
-              if(optionValue){
-                  $(".week").not("." + optionValue).hide();
-                  $("." + optionValue).show();
-              } else{
-                  $(".week").hide();
-              }
-            });
+( function($) {
+      $(document).ready(function(){
+        $("select").change(function(){
+          $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue){
+                $(".week").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else{
+                $(".week").hide();
+            }
+          });
         }).change();
+        
+        $('.table').footable();
       });
     } ) ( jQuery );
 
