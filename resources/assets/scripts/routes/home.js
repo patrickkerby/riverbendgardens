a37e3797bcbf2ad2ee50b41977ccc2e497737ef5
Patrick Kerby <patrick@kerbydesign.com>
2020-02-04 17:48:41 -0700

    set map js to only fire on home page

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -6,6 +6,24 @@ export default {
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
     
+    $(document).ready(function(){
+      
+      mapboxgl.accessToken = 'pk.eyJ1IjoicGF0cmlja2tlcmJ5IiwiYSI6ImpxWDBaVFkifQ.t3gbX7-Sfy3Z9Nh14aLFow';
+  
+      var bounds = [
+        [-114.694127, 53.101844], // Southwest coordinates
+        [-112.524187, 53.753422],  // Northeast coordinates
+      ];
+     
+      var map = new mapboxgl.Map({
+        container: 'map',
+        height: '680px',
+        style: 'mapbox://styles/patrickkerby/cjexbfjfi358d2rpeom12bmuo',
+        maxBounds: bounds,
+      });
     
+      map.scrollZoom.disable();
+      map.addControl(new mapboxgl.NavigationControl());
+    });
   },
 };
