4c9d30d1587301422857341660177a247a498fa2
Patrick Kerby <patrick@kerbydesign.com>
2020-03-04 12:21:40 -0700

    Veggie Guide Detail: Responsive, plus weird modal jankiness

--- resources/views/veggies.blade.php
+++ resources/views/veggies.blade.php
@@ -20,6 +20,7 @@
             </a>
             <div class="modal fullmodal" id="pickupModal-{{ $loop->index }}" tabindex="-1" role="dialog" aria-hidden="true">
               <div class="modal-dialog" role="document">
+                <div class="modal-content">
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
@@ -50,7 +51,8 @@
                         </div>
                       </div>
                     </div>
-                  </div>										
+                  </div>
+                </div>
               </div>
             </div>
           </div>
