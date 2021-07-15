760c3ff9f462e9aa1660d28b51e63a766edb9b29
Patrick Kerby <patrick@kerbydesign.com>
2021-07-15 14:45:58 -0600

    Display week with date attempt6

--- resources/views/lists-home.blade.php
+++ resources/views/lists-home.blade.php
@@ -185,12 +185,12 @@ $pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the co
             @switch($currentCSAWeek)
               @case(1)
                   
-                @php $weekDate = date($week1_row['week'];) @endphp);
+                @php $weekDate = date($week1_row['week']); @endphp);
                 {{ $weekDate }}
                 @break
 
               @case(2)
-                @php $weekDate = date($week2_row['week'];) @endphp
+                @php $weekDate = date($week2_row['week']); @endphp
                 {{ $weekDate }}
                 @break
 
