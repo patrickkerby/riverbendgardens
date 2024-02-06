30422c0811c661ef113609d83da98ff1a5b090ac
Patrick Kerby <patrick@kerbydesign.com>
2024-02-05 17:16:17 -0700

    udpate product pages

--- app/filters.php
+++ app/filters.php
@@ -190,4 +190,28 @@ function custom_checkout_field_referral( $checkout ) {
     }
     
     
-add_filter( 'woocommerce_cart_needs_shipping', '__return_true' );
+add_filter( 'woocommerce_checkout_fields' , 'App\bbloomer_simplify_checkout_virtual' );
+ 
+function bbloomer_simplify_checkout_virtual( $fields ) {
+    
+   $only_virtual = true;
+    
+   foreach( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+      // Check if there are non-virtual products
+      if ( ! $cart_item['data']->is_virtual() ) $only_virtual = false;   
+   }
+     
+    if( $only_virtual ) {
+       unset($fields['billing']['billing_company']);
+       unset($fields['billing']['billing_address_1']);
+       unset($fields['billing']['billing_address_2']);
+       unset($fields['billing']['billing_city']);
+       unset($fields['billing']['billing_postcode']);
+       unset($fields['billing']['billing_country']);
+       unset($fields['billing']['billing_state']);
+       unset($fields['billing']['billing_phone']);
+       add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
+     }
+     
+     return $fields;
+}
