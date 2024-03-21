b9d95adb99943ca2cd7fc5052b13964d94b48ac7
Patrick Kerby <patrick@kerbydesign.com>
2024-03-20 18:01:30 -0600

    fix page builder photo gallery

--- resources/views/partials/flexible-gallery.blade.php
+++ resources/views/partials/flexible-gallery.blade.php
@@ -1,16 +1,18 @@
 <section class="gallery row no-gutters">
-  <div class="grid-layout col-md-12">
+  {{-- <div class="grid-layout "> --}}
     @php 
 
       $images = get_sub_field('gallery');
       
       if( $images ): @endphp
               @php foreach( $images as $image ): @endphp
-                  <div class="grid-item image">
-                      <a href="@php echo $image['url']; @endphp" target="_blank" style="background-image: url('@php echo $image['sizes']['large']; @endphp');"></a>
+                  <div class="grid-item image col-sm">
+                      <a href="@php echo $image['url']; @endphp" target="_blank" style="background-image: url('@php echo $image['sizes']['large']; @endphp');">
+                        <img src="{{ $image['sizes']['medium'] }}" alt="{{ $image['alt'] }}" />
+                      </a>
                   </div>
               @php endforeach; @endphp
       @php endif;		
     @endphp
-  </div>  
+  {{-- </div>   --}}
 </section>
