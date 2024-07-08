8e0672ef6700e6770008d3060e5ff8a531c2354c
Patrick Kerby <patrick@kerbydesign.com>
2024-07-08 14:34:21 -0600

    fix route list (normalize location meta)

--- resources/views/list-routes.blade.php
+++ resources/views/list-routes.blade.php
@@ -8,10 +8,8 @@
 @php
 //List of global variables
 global $wpdb, $woocommerce;
-$pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the composite weeks by component ID! This is special.
 
 // ------------------------- Get pickup dates for current year, set in Global Options -------------------------    
-// TODO: see if these dates can be provided on composite product item _rather_ than global options.
 
   $rows = get_field('pickup_dates','options' );
 
@@ -64,7 +62,7 @@ $pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the co
   $currentWeek = date('W');
   $currentCSAWeek = $currentWeek - 28;
 
-  if ($currentCSAWeek === 0) {
+  if ($currentCSAWeek == 0) {
     $currentCSAWeek = 1;
   }
 
@@ -91,10 +89,6 @@ $pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the co
     $displayBiwk = true;
   }
 
-  $year = "CSA 2020 - 14 week";
-  $school = "School CSA";
-  $late_season = "Late Season CSA";
-
   $bigger_count = 0;
   $smaller_count = 0;  
 
@@ -106,7 +100,7 @@ $pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the co
   $smaller_crates_total = 0;
   $bigger_crates_total = 0;
 
-////********** Query the orders! 
+//** Query the orders! 
   $args = array(
       'orderby' => 'date',
       'order' => 'DESC',
@@ -116,25 +110,10 @@ $pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the co
 
   $product_id_15wk = '5958';
   $product_id_biwk = '87354';
-  $product_id_weekly = '59432';
 
-//first get all the order ids
+  //first get all the order ids
   $query = new WC_Order_Query( $args );
-  $order_ids = $query->get_orders();
-
-  $filtered_order_ids_biwk = array();
-  $filtered_order_ids_15wk = array();
-
-  //Weeklies
-  $weekly_components = array();
-  $i = 1;
-  foreach ($pickup_weeks as $component_id => $component_data ) {
-      $weekly_components['week' . $i]['title'] = $component_data['title'];
-      $weekly_components['week' . $i]['id'] = $component_data['component_id'];
-      $i++;
-  }
-  $current_component_id = $weekly_components[$week_in_season]['id'];
-  $current_component_title = $weekly_components[$week_in_season]['title'];
+  $orders = $query->get_orders();
 
 @endphp
 
@@ -250,7 +229,7 @@ $pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the co
                 Week 1: {{ $weekDate }}
             @endswitch
           </h2>
-          <p>Clear = Bigger  /  White = Smaller.<br /> These numbers <em>include</em> weeklies and extras.</p>
+          <p>Clear = Bigger  /  White = Smaller.<br /> These numbers <em>include</em> extras.</p>
           <table class="table footable">
             <thead>
               <tr>
@@ -264,64 +243,52 @@ $pickup_weeks = get_post_meta(59432, '_bto_data', true); // This gets all the co
             </thead>
             <tbody>
               @foreach ($select_locations as $item)
-                @php
-                  $weekly_count = 0;
+                @php                
                   $biweekly_total_count = 0;
                   $fullseason_smaller_count = 0;
-                  $fullseason_bigger_count = 0;
-                  $weekly_location_count_bigger = 0;
-                  $weekly_location_count_smaller = 0;	 
+                  $fullseason_bigger_count = 0;                  
                   
                   $location = $item['location']->slug;
                   $location_name = $item['location']->name;
 
                   $extras = $item['extras'];
 
-                  if ($extras === true) {
-										$extras = 2;
+                  if ($extras == true) {
+										$extras = 1;
 									}
                   else {
                     $extras = 0;
                   }        
                         
-                  foreach ($order_ids as $order_id) {
-                    $order = wc_get_order($order_id);
+                  foreach ($orders as $order) {
+                    
                     $order_items = $order->get_items();
 
                     foreach ($order_items as $order_item) {
-                      $filtered_location = $order_item->get_meta('pa_pickup-location'); //biweekly and weekly use global attributes
-                      $filtered_location_15wk = $order_item->get_meta('location'); //15 wk uses custom attributes
-                      $filtered_size = $order_item->get_meta('size'); //15 wk uses custom attributes
+                      $filtered_location = $order_item->get_meta('pa_pickup-location');
+                      $filtered_size = $order_item->get_meta('size');
                       $filtered_quantity = $order_item->get_quantity();
-                      $filtered_component = $order_item->get_meta('_composite_item'); //15wk uses custom attributes                      
 
-                      if ($filtered_component == $current_component_id && $filtered_location == $location && $filtered_size === 'Smaller') {
-                        $weekly_location_count_smaller += $filtered_quantity;
-                      }
-                      elseif ($filtered_component == $current_component_id && $filtered_location == $location && $filtered_size === 'Bigger') {
-                        $weekly_location_count_bigger += $filtered_quantity;
-                      }
-                      elseif ($order_item->get_product_id() == $product_id_biwk && $filtered_location == $location) {
+                      if ($order_item->get_product_id() == $product_id_biwk && $filtered_location == $location) {
                         $biweekly_total_count += $filtered_quantity;
                       }
-                      elseif ($order_item->get_product_id() == $product_id_15wk && $filtered_location_15wk == $location_name && $filtered_size === 'Bigger') {
-                        $fullseason_bigger_count += $filtered_quantity;
+                      elseif ($order_item->get_product_id() == $product_id_15wk && $filtered_location == $location && $filtered_size == 'Bigger') {
+                        $fullseason_bigger_count += $filtered_quantity;                        
                       }
-                      elseif ($order_item->get_product_id() == $product_id_15wk && $filtered_location_15wk == $location_name && $filtered_size === 'Smaller') {
+                      elseif ($order_item->get_product_id() == $product_id_15wk && $filtered_location == $location && $filtered_size == 'Smaller') {
                         $fullseason_smaller_count += $filtered_quantity;
                       }
                     }
                   }
                   
-                  if ($displayBiwk === true) {
-                    $bigger_count = $fullseason_bigger_count + $biweekly_total_count + $weekly_location_count_bigger + $extras;
+                  if ($displayBiwk == true) {
+                    $bigger_count = $fullseason_bigger_count + $biweekly_total_count + $extras;
                   }
                   else {
-                    $bigger_count = $fullseason_bigger_count + $weekly_location_count_bigger + $extras;
+                    $bigger_count = $fullseason_bigger_count + $extras;
                   }
 
-                  $smaller_count = $fullseason_smaller_count + $weekly_location_count_smaller;
-
+                  $smaller_count = $fullseason_smaller_count;
                   $bigger_crates = $bigger_count/2;
                   $smaller_crates = $smaller_count/2; 
                 @endphp
