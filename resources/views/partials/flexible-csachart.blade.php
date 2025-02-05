ae48c67f203c8cc7f0d61d99fb4e0790e3d624d9
Patrick Kerby <patrick@kerbydesign.com>
2025-02-05 12:23:41 -0700

    Add csa chart to shop page

--- resources/views/partials/flexible-csachart.blade.php
+++ resources/views/partials/flexible-csachart.blade.php
@@ -10,8 +10,8 @@
       <p>{!! $description !!}</p>
   @else 
     <h2>What to expect in your CSA!</h2>
-    <p>Below is a list of all veggies that went out in last year's CSA. Although this year won't neccessarily be the same (due to weather, etc.), it should give you a pretty good idea of variety and quantity.</p>
-    <p>The chart shows veggies included in our “Bigger” bounty size. The main difference between our Bigger and Smaller sizes is that carrots and potatoes are typically delivered every week in “Bigger” bounties while they alternate weeks in “Smaller” bounties.</p>
+    <p>Below is a list of most of the veggies that went out in last year's CSA. Although this year won't neccessarily be the same (due to weather, etc.), it should give you a pretty good idea of variety and quantity.</p>
+    <p>The chart shows veggies included in our Bigger bounty size. The main difference between our Bigger and Smaller sizes is that carrots and potatoes are typically delivered every week in Bigger bounties while they alternate weeks in Smaller bounties.</p>
   @endif
   </div>
   <table class="col-sm-11 col-md-10">
